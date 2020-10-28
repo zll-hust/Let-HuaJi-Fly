949d56d0085afddf7464f646114b22abb54e39ca
zll-hust <zh20010728@126.com>
2020-10-28 20:47:20 +0800

    start!

---
+++ src/com/zxl/Main/Main.java
@@ -0,0 +1,12 @@
+package com.zxl.Main;
+
+/**
+ * Description:
+ * 主函数运行类，项目入口
+ * @encode UTF-8
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("start a new project!");
+    }
+}
