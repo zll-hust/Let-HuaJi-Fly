a3128760091c12edb76d73f387b7a6f7c0e3a425
lemon <64292231+lemon-ux@users.noreply.github.com>
2020-11-08 15:17:52 +0800

    Update PaintPanel.java

--- src/com/zxl/PaintPanel.java
+++ src/com/zxl/PaintPanel.java
@@ -10,7 +10,7 @@ import javax.swing.JPanel;
 
 /**
  * Description:
- * 也不知道是啥，好像是把角色画到背景上的一个类
+ * 鎶婅鑹茬敾鍒拌儗鏅笂鐨勭被
  *
  * @encode UTF-8
  */
@@ -34,12 +34,6 @@ public class PaintPanel extends JPanel {
 
         for (Role nowPainting : roles) {
             if (nowPainting != null) {
-//                    System.out.println(nowPainting.color + " " + nowPainting.getID());
-                /*if (nowPainting.type == 1) {
-                    //g.setColor(Color.decode(nowPainting.color));
-                    g.fillOval((int)nowPainting.getX() - (int)nowPainting.getR(), (int)nowPainting.getY() - (int)nowPainting.getR(),
-                            nowPainting.getD(), nowPainting.getD());
-                }else{       */
                     Image image = Toolkit.getDefaultToolkit().getImage(Role.imgPath[nowPainting.type]);
                     BufferedImage img = MyUtils.Rotate(image, nowPainting.angle);
                     g.drawImage(img, (int)nowPainting.getX() - (int)nowPainting.getR(), (int)nowPainting.getY() - (int)nowPainting.getR(), this);
